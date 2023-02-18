246c5b4d5d01fab01a68d50d1f84a02776371ee5
GUSTAV0DEDEUS <88512041+GUSTAV0DEDEUS@users.noreply.github.com>
2023-02-17 21:17:35 -0300

    desenvolvimento

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+var dkl1n = {
+  nome: "DKL1N",
+  img: "",
+  campeao: "",
+  placar: [0, 0, 0],
+  tempo: 40,
+  w_d: ["win", "defeat"]
+}
+
+function openModal(){
+  var modal = document.getElementById('modalID')
+  modal.style.display = 'grid';
+}
+
+function closeModal(){
+  var modal = document.getElementById('modalID')
+  modal.style.display = 'none';
+}
+
+function adicionarPartida(){
+
+}
